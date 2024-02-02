083e5721106c77d299164efac9c08a5ae4513d95
YupetEmmanuel <emmanuelsworo0987@gmail.com>
2024-02-02 14:46:28 +0300

    first commit

---
+++ hjhj.py
@@ -0,0 +1,9 @@
+name=90
+if name < 89:
+    result='go fuck yourself'
+elif name <= 70:
+   result = 'much ablaged'
+else:
+    result= 'go and chaboink'
+
+print(result)
---
+++ lists/list_operation.py
@@ -0,0 +1,20 @@
+list1=['a', True, 3, 4.5, 3]
+list2=[100, False, 'John', 0.0, True]
+
+#Index before update
+
+print("Values before updates :", list2)
+
+#updating index 2 in a list2 
+
+Newvalue="Doe"
+list2[1]=Newvalue
+
+print("values after updates: ", list2)
+
+
+
+
+
+
+
