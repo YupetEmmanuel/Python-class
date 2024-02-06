9a830cfe499c71fab1efb81095bdfba61c465aa5
YupetEmmanuel <emmanuelsworo0987@gmail.com>
2024-02-06 16:10:46 +0300

    day

---
+++ functions/functions/functions.py
@@ -0,0 +1,21 @@
+def name():
+    clam=input("enter your name is you may : ")
+    return clam
+
+
+
+def age():
+    Age=input("Enter your age if you may : " )
+    print(Age,('\n'),name()) 
+age() 
+
+
+
+def add(num1,num2):
+    Answer=num1+num2
+    return Answer
+add(6,6)
+  
+  
+
+
